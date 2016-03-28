0ef0e4deaba8f48e7bb67367028ef930ff9efa5e
Mark Rickert <mjar81@gmail.com>
2016-03-28 14:53:03 -0400

    Make sure the redux state is immutable

--- App/Reducers/SettingsReducer.js
+++ App/Reducers/SettingsReducer.js
@@ -10,7 +10,7 @@ export const INITIAL_STATE = Immutable({
 })
 
 const save = (state, action) => {
-  return { ...state, ...action}
+  return Immutable({ ...state, ...action})
 }
 
 // map our types to our handlers
