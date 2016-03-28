1979a78716a15567b2824b58365910d88a7fb556
Mark Rickert <mjar81@gmail.com>
2016-03-28 17:30:37 -0400

    Remove extra files that aren't used.

--- App/Reducers/index.js
+++ App/Reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux'
-import LoginReducer from './LoginReducer'
-import WeatherReducer from './WeatherReducer'
 import SettingsReducer from './SettingsReducer'
 
 // glue all the reducers together into 1 root reducer
 export default combineReducers({
-  login: LoginReducer,
-  weather: WeatherReducer,
   settings: SettingsReducer
 })
 
--- App/Sagas/index.js
+++ App/Sagas/index.js
@@ -1,10 +1,6 @@
 import { watchStartup } from './StartupSaga'
-import { watchLoginAttempt } from './LoginSaga'
-import { watchWeatherRequest } from './WeatherSaga'
 
 // start the daemons
 export default [
-  watchStartup,
-  watchLoginAttempt,
-  watchWeatherRequest
+  watchStartup
 ]
